9172ee9a7cbd30f769862df4d9835b3f474af61e
Huahuoao <2294198058@qq.com>
2022-10-03 22:57:51 +0800

    新建项目

---
+++ src/main/java/com/huahuo/freezeframepostoffice/FreezeFramePostOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.huahuo.freezeframepostoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreezeFramePostOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreezeFramePostOfficeApplication.class, args);
+    }
+
+}
