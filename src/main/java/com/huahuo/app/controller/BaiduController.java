52c7a923dc1f482e1f1e5e4b5a7e3c9dc854755d
huahuo_ubt <2294198058@qq.com>
2022-10-04 21:57:12 +0800

    test

--- src/main/java/com/huahuo/app/controller/BaiduController.java
+++ src/main/java/com/huahuo/app/controller/BaiduController.java
@@ -1,16 +1,24 @@
 package com.huahuo.app.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.baidu.aip.imagesearch.AipImageSearch;
 import com.huahuo.app.common.R;
 import com.huahuo.app.service.BaiduService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.Record;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @作者 花火
@@ -28,13 +36,32 @@ public class BaiduController {
 
     private AipImageSearch client = new AipImageSearch(APP_ID, API_KEY, SECRET_KEY);
     @PostMapping("/upload")
-    public R<String> upload(@RequestParam String url)
+    public void upload(@RequestParam String url)
     {
-       return R.success( baiduService.similarAdd(new AipImageSearch(APP_ID, API_KEY, SECRET_KEY),url));
+        String text =  baiduService.similarAdd(client,url);
+        System.out.println(text);
     }
     @PostMapping("/search")
-    public String search(@RequestParam String url)
+    public void search(@RequestParam String url)
     {
-        return baiduService.similarSearch(client,url);
+
+        String text = baiduService.similarSearch(client,url);
+        JSONArray jsonArray = JSON.parseObject(text).getJSONArray("result");
+        //填充初始数据，此处过程省略
+        List<JSONObject> jsonObjectList = jsonArray.toJavaList(JSONObject.class);
+        Map<String, String> map = jsonObjectList.stream().filter(Objects::nonNull)
+                .collect(Collectors.toMap(item -> item.getString("score"), item -> item.getString("cont_sign")));
+        System.out.println(map);
+        int i=3;
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            if(i==0) break;
+            i--;
+            String mapKey = entry.getKey();
+            String mapValue = entry.getValue();
+            System.out.println(mapKey+":"+mapValue);
+        }
+
+//        JSONObject jsonObject= (JSONObject) JSON.parse("result");
+//        System.out.println(jsonObject);
     }
 }
--- src/main/java/com/huahuo/app/service/BaiduService.java
+++ src/main/java/com/huahuo/app/service/BaiduService.java
@@ -15,13 +15,14 @@ public class BaiduService {
     public String similarAdd(AipImageSearch client,String url) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
-        String brief = "{\"name\":\"周杰伦\", \"id\":\"666\"}";
-        options.put("brief","{\"name\":\"周杰伦\", \"id\":\"666\"}");
+        String brief = "default";
+        options.put("brief","default");
         options.put("method","post");
         options.put("Content-Type","application/x-www-form-urlencoded");
         // 相同图检索—入库, 图片参数为远程url图片
         JSONObject res = client.similarAddUrl(url,brief, options);
-       return res.toString(2);
+        System.out.println(res);
+        return res.toString(2);
 
     }
     public String similarSearch(AipImageSearch client,String url) {
@@ -31,11 +32,8 @@ public class BaiduService {
 //        options.put("tag_logic", "0");
 //        options.put("pn", "100");
 //        options.put("rn", "250");
-
-
-       JSONObject res = client.similarSearchUrl(url, options);
+          JSONObject res = client.similarSearchUrl(url, options);
           return res.toString(2);
-
     }
 
 
