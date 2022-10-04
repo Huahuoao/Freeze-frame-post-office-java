5243c3905eab34872342d763cf24abe13b6c587c
Huahuoao <2294198058@qq.com>
2022-10-04 22:55:34 +0800

    优化了百度识图服务

--- src/main/java/com/huahuo/app/service/BaiduService.java
+++ src/main/java/com/huahuo/app/service/BaiduService.java
@@ -1,18 +1,32 @@
 package com.huahuo.app.service;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
 import com.baidu.aip.imagesearch.AipImageSearch;
+import com.huahuo.app.utils.JSONUtils;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @作者 花火
  * @创建日期 2022/10/4 11:03
  */
+
+/**
+ * 返回id
+ */
 @Service
 public class BaiduService {
-    public String similarAdd(AipImageSearch client,String url) {
+    public static final String APP_ID ="27724400";
+    public static final String API_KEY ="Oumu51qBYNYBOhoelLn5h3OH";
+    public static final String SECRET_KEY ="KBKRvMa23mbXPzEHAGqfoGVOWjTOveOw";
+
+    private AipImageSearch client = new AipImageSearch(APP_ID, API_KEY, SECRET_KEY);
+    public String similarAdd(String url) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
         String brief = "default";
@@ -21,19 +35,47 @@ public class BaiduService {
         options.put("Content-Type","application/x-www-form-urlencoded");
         // 相同图检索—入库, 图片参数为远程url图片
         JSONObject res = client.similarAddUrl(url,brief, options);
-        System.out.println(res);
-        return res.toString(2);
+        HashMap<String, String> hashMap = JSONUtils.JsonObjectToHashMap(res);
+        String cont_sign = hashMap.get("cont_sign");
+        return  cont_sign;
 
     }
-    public String similarSearch(AipImageSearch client,String url) {
+
+    /**
+     * 返回一个list(3)
+     * list里面是个map
+     * map是   ”id“：
+     *        ”value“：
+     * @param url
+     * @return
+     */
+    public ArrayList<HashMap<String,String>> similarSearch(String url) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
 //        options.put("tags", "100,11");
 //        options.put("tag_logic", "0");
 //        options.put("pn", "100");
 //        options.put("rn", "250");
-          JSONObject res = client.similarSearchUrl(url, options);
-          return res.toString(2);
+        ArrayList<HashMap<String,String>> arrayList = new ArrayList<>();
+         JSONObject res = client.similarSearchUrl(url, options);
+         String text=res.toString(2);
+         JSONArray jsonArray = JSON.parseObject(text).getJSONArray("result");
+        //填充初始数据，此处过程省略
+        List<com.alibaba.fastjson2.JSONObject> jsonObjectList = jsonArray.toJavaList(com.alibaba.fastjson2.JSONObject.class);
+         Map<String, String> map = jsonObjectList.stream().filter(Objects::nonNull)
+                .collect(Collectors.toMap(item -> item.getString("score"), item -> item.getString("cont_sign")));
+        int i=3;
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            if(i==0) break;
+            i--;
+            String mapKey = entry.getKey();
+            String mapValue = entry.getValue();
+            HashMap<String,String> temp = new HashMap<>();
+            temp.put("id",mapValue);
+            temp.put("value",mapKey);
+            arrayList.add(temp);
+        }
+           return arrayList;
     }
 
 
